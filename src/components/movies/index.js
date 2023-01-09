c9714c72eae3e96759661f828ba23742b4261e42
ChenCheng-0715 <bendelayoc@gmail.com>
2023-01-09 14:52:58 +0800

    feat: 添加http请求

--- src/components/movies/index.js
+++ src/components/movies/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
+import Movie from './movie'
 
 const Movies = () => {
+    const [movies,setMovies] = useState([])
+    const [loading,setLoading] = useState(false)
+
+    useEffect(()=>{
+        setLoading(true)
+        axios.get(`https://api.themoviedb.org/3/discover/tv`, {
+            params: {
+              sort_by: 'popularity.desc',
+              api_key: process.env.API_KEY
+            }
+          })
+            .then( (response) => {
+              setLoading(false);
+              setMovies(response?.data?.results);
+            })
+            .catch( (error) => {
+              setLoading(false);
+              console.log(error);
+            })
+    },[])
+
     return (
         <div className='flex flex-wrap -mb-4'>
-            hello
+            {
+              !loading && movies?.length 
+              ? movies.map((movie)=> <Movie key={movie.id} movie={movie}/>)
+              : <h2>Loading</h2>
+            }
         </div>
     )
 }
